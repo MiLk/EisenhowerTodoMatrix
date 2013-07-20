9e60672189cfce5302f4046ba0f7dbd7227566d0
Emilien Kenler <hello@emilienkenler.com>
2013-07-20 13:35:26 +0200

    Add CouchDB backend
    Signed-off-by: Emilien Kenler <hello@emilienkenler.com>

--- README.md
+++ README.md
@@ -1,3 +1,34 @@
 # Eisenhower Todo Matrix
 
 This HTML & Javascript application allow you to organize your tasks and stop waste your time.
+
+## Configuring CouchDB
+
+To use CouchDB 1.3 with jQuery, you have to enable CORS feature in config.
+
+Prior to 1.3, CORS is not available, so you can use a `cors-proxy` <https://github.com/gr2m/CORS-Proxy>.
+
+``` javascript
+var cors_proxy = require("corsproxy");
+var http_proxy = require("http-proxy");
+cors_proxy.options = {
+     target: "http://127.0.0.1:5984"
+};
+http_proxy.createServer(cors_proxy).listen(5984,'your_ip');
+```
+
+You have to create an empty database with the following document:
+``` json
+{
+   "UI": [
+   ],
+   "NUI": [
+   ],
+   "UNI": [
+   ],
+   "NUNI": [
+   ],
+   "done": [
+   ]
+}
+```
--- js/app.js
+++ js/app.js
@@ -1,16 +1,53 @@
-function MatrixCtrl($scope) {
+function onError(data, status, headers, config) {
+  console.error(status);
+  console.error(data);
+};
+
+function MatrixCtrl($scope, $http) {
+
+  var api_url = 'http://couchdb.emilienkenler.com:5984/eisenhower';
+
+  $scope.docID = null;
   $scope.tasks = {
     'UI': [],
     'NUI': [],
     'UNI': [],
     'NUNI': [],
-    'done': []
+    'done': [],
+    'id': 1
   };
-  $scope.id = 1;
+
+  $http({
+    method: 'GET',
+    url: api_url + '/_all_docs'
+  }).success(function(data, status, headers, config) {
+    $scope.docID = data.rows[0].id;
+    $http({
+      method: 'GET',
+      url: api_url + '/' + $scope.docID
+    }).success(function(data, status, headers, config) {
+        $scope.tasks = data;
+        $scope.$watch('tasks', function(newValue, oldValue) {
+          if(  oldValue.UI.length == newValue.UI.length
+            && oldValue.UNI.length == newValue.UNI.length
+            && oldValue.NUI.length == newValue.NUI.length
+            && oldValue.NUNI.length == newValue.NUNI.length
+            && oldValue.done.length == newValue.done.length
+            ) return;
+          $http({
+            method: 'PUT',
+            url: api_url + '/' +  $scope.docID,
+            data: $scope.tasks
+          }).success(function(data, status, headers, config) {
+            console.log('Saved');
+          }).error(onError);
+        }, true);
+    }).error(onError);
+  }).error(onError);
 
   $scope.submit = function (list) {
     if (this.text) {
-      $scope.tasks[list].push({ id: ($scope.id++), name: this.text });
+      $scope.tasks[list].push({ id: ($scope.tasks.id++), name: this.text });
       this.text = '';
       $scope['showForm' + list] = !$scope['showForm' + list];
     }
