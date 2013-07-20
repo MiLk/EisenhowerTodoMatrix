a5f0a7657485400a5bacfbd9584790ab32f71567
Emilien Kenler <hello@emilienkenler.com>
2013-07-20 09:41:29 +0200

    Use AngularJS
    Signed-off-by: Emilien Kenler <hello@emilienkenler.com>

--- js/app.js
+++ js/app.js
@@ -1,27 +1,31 @@
-$(document).ready(function(){
-  // Display form to add a task on dblclick in a cell
-  $('button.btn').on('click', function(e) {
-    $('form').hide();
-    $(this).closest('td').find('form').toggle();
-    $(this).closest('td').find('form input[type="text"]').focus();
-    e.preventDefault();
-    return false;
-  });
-  // Add task
-  $('form').on('submit', function(e) {
-    var val = $(this).find('input[type="text"]').val();
-    $li = $('<li>'+val+'</li>');
-    $li.on('click', deleteTask);
-    $li.prependTo($(this).parent().find('ul'));
-    $(this).toggle();
-    $(this).find('input[type="text"]').val('');
-    e.preventDefault();
-    return false;
-  });
-  $('form').hide();
+function MatrixCtrl($scope) {
+  $scope.tasks = {
+    'UI': [],
+    'NUI': [],
+    'UNI': [],
+    'NUNI': [],
+    'done': []
+  };
+  $scope.id = 1;
 
-  function deleteTask() {
-    $this = $(this);
-    $this.addClass('deleted');
+  $scope.submit = function (list) {
+    if (this.text) {
+      $scope.tasks[list].push({ id: ($scope.id++), name: this.text });
+      this.text = '';
+      $scope['showForm' + list] = !$scope['showForm' + list];
+    }
   }
-});
+
+  $scope.remove = function ($event) {
+    var id = $($event.target).attr('data-id');
+    var list = $($event.target).attr('data-list');
+    _.filter($scope.tasks[list],function (task) {
+      return task.id == id;
+    }).forEach(function (task) {
+        $scope.tasks['done'].push(task);
+      });
+    $scope.tasks[list] = _.reject($scope.tasks[list], function (task) {
+      return task.id == id;
+    });
+  }
+}
