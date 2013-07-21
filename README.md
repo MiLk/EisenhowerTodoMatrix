33d2a5864d3c38ad6cd58137acf8e251b4e96e8f
Emilien Kenler <hello@emilienkenler.com>
2013-07-21 09:19:24 +0200

    Multiple matrices. Possibility to create new matrices.
    Signed-off-by: Emilien Kenler <hello@emilienkenler.com>

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 This HTML & Javascript application allow you to organize your tasks and stop waste your time.
 
+The Eisenhower Matrix is one of the simplest tools that can quickly make you a lot better at managing your time.
+
+This version use AngularJS client-side and the persistence is provided by a CouchDB server. There is no server application or API, except for the CouchDB built-in API.
+
 ## Configuring CouchDB
 
 To use CouchDB 1.3 with jQuery, you have to enable CORS feature in config.
@@ -17,18 +21,40 @@ cors_proxy.options = {
 http_proxy.createServer(cors_proxy).listen(5984,'your_ip');
 ```
 
-You have to create an empty database with the following document:
-``` json
-{
-   "UI": [
-   ],
-   "NUI": [
-   ],
-   "UNI": [
-   ],
-   "NUNI": [
-   ],
-   "done": [
-   ]
-}
+You have to create the database and the view:
 ```
+curl -i -X PUT http://my.couchdb.server:5984/eisenhower
+curl -i -X PUT http://my.couchdb.server:5984/eisenhower/_design/users -d @couchdb/users.json
+
+```
+
+## Configuring Application
+
+Just modify the `api_url` variable in `js/app.js`.
+
+## Usage
+
+Just open the HTML file in your browser. (Must be served via http protocole and not file://).
+
+## License
+
+The MIT License (MIT)
+
+Copyright (c) 2013 Emilien Kenler <hello@emilienkenler.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
--- js/app.js
+++ js/app.js
@@ -1,13 +1,20 @@
+angular.module('Matrix', [])
+  .config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.
+      when('/', { templateUrl: 'partials/home.html', controller: HomeCtrl }).
+      when('/todo/:docId', { templateUrl: 'partials/matrix.html', controller: MatrixCtrl }).
+      otherwise({ redirectTo: '/' });
+  }]);
+
 function onError(data, status, headers, config) {
   console.error(status);
   console.error(data);
 };
 
-function MatrixCtrl($scope, $http) {
+var api_url = 'http://my.couchdb.server:5984/eisenhower';
 
-  var api_url = 'http://couchdb.emilienkenler.com:5984/eisenhower';
-
-  $scope.docID = null;
+function MatrixCtrl($scope, $http, $routeParams) {
+  $scope.docID = $routeParams.docId;
   $scope.tasks = {
     'UI': [],
     'NUI': [],
@@ -17,33 +24,27 @@ function MatrixCtrl($scope, $http) {
     'id': 1
   };
 
-  $http({
-    method: 'GET',
-    url: api_url + '/_all_docs'
-  }).success(function(data, status, headers, config) {
-    $scope.docID = data.rows[0].id;
-    $http({
-      method: 'GET',
-      url: api_url + '/' + $scope.docID
-    }).success(function(data, status, headers, config) {
-        $scope.tasks = data;
-        $scope.$watch('tasks', function(newValue, oldValue) {
-          if(  oldValue.UI.length == newValue.UI.length
-            && oldValue.UNI.length == newValue.UNI.length
-            && oldValue.NUI.length == newValue.NUI.length
-            && oldValue.NUNI.length == newValue.NUNI.length
-            && oldValue.done.length == newValue.done.length
-            ) return;
-          $http({
-            method: 'PUT',
-            url: api_url + '/' +  $scope.docID,
-            data: $scope.tasks
-          }).success(function(data, status, headers, config) {
-            console.log('Saved');
-          }).error(onError);
-        }, true);
-    }).error(onError);
-  }).error(onError);
+      $http({
+        method: 'GET',
+        url: api_url + '/' + $scope.docID
+      }).success(function (data, status, headers, config) {
+          $scope.tasks = data;
+          $scope.$watch('tasks', function (newValue, oldValue) {
+            if (oldValue.UI.length == newValue.UI.length
+              && oldValue.UNI.length == newValue.UNI.length
+              && oldValue.NUI.length == newValue.NUI.length
+              && oldValue.NUNI.length == newValue.NUNI.length
+              && oldValue.done.length == newValue.done.length
+              ) return;
+            $http({
+              method: 'PUT',
+              url: api_url + '/' + $scope.docID,
+              data: $scope.tasks
+            }).success(function (data, status, headers, config) {
+                console.log('Saved');
+              }).error(onError);
+          }, true);
+        }).error(onError);
 
   $scope.submit = function (list) {
     if (this.text) {
@@ -66,3 +67,33 @@ function MatrixCtrl($scope, $http) {
     });
   }
 }
+
+function HomeCtrl($scope, $http) {
+  $scope.users = [];
+
+  $http({
+    method: 'GET',
+    url: api_url + '/_design/users/_view/list'
+  }).success(function (data, status, headers, config) {
+      $scope.users = data.rows;
+    }).error(onError);
+
+  $scope.new = function() {
+    var doc = {
+      'UI': [],
+      'NUI': [],
+      'UNI': [],
+      'NUNI': [],
+      'done': [],
+      'id': 1,
+      'user': $('#inputName').val()
+    };
+    $http({
+      method: 'POST',
+      url: api_url + '/',
+      data: doc
+    }).success(function (data, status, headers, config) {
+        window.location = '/#/todo/'+data.id;
+    }).error(onError);
+  }
+};
